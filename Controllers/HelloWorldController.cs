a0945f00ab39a6557e57f1978b386558ec9eb9a8
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-26 14:18:40 -0500

    Inyeccion de dependencias

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloWorldController: ControllerBase
+{
+    IHelloWorldService helloWorldService;
+
+    public HelloWorldController(IHelloWorldService helloWorld)
+    {
+        helloWorldService = helloWorld;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(helloWorldService.GetHelloWorld());
+    }
+}
--- Middlwares/TimeMiddleware.cs
+++ Middlwares/TimeMiddleware.cs
@@ -16,13 +16,11 @@ public class TimeMiddleware
 
     public async Task Invoke(Microsoft.AspNetCore.Http.HttpContext context)
     {
-         await next(context);
+        await next(context);
         if (context.Request.Query.Any(p => p.Key == "time"))
         {
             await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
         }
-
-       
     }
 }
 
