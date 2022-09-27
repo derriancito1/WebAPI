ce40f51ceba38bedb3afebec685bf887b894caa7
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-27 16:36:00 -0500

    Inyectando servicios como dependencias

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 //builder.Services.AddScoped<IHelloWorldService, HelloworldService>();
+
 builder.Services.AddScoped<IHelloWorldService>(p=> new HelloworldService());
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITareasService, TareasService>();
 
 var app = builder.Build();
 
