a3cede4c21a2383bcda31be1c5df962d6ffdcf29
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-27 18:07:17 -0500

    Generando base de datos con EF

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using WebAPI;
 using WebAPI.Middlwares;
 using WebAPI.Services;
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<TareasContext>("Data Source=127.0.0.1; Initial Catalog=TareasDb; user=root; password=123");
 //builder.Services.AddScoped<IHelloWorldService, HelloworldService>();
 
 builder.Services.AddScoped<IHelloWorldService>(p=> new HelloworldService());
